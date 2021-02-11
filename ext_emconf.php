19a100172a7cf637e8b20f5885a40a976d8d5f8f
Michael Fladischer <FladischerMichael@fladi.at>
2021-02-11 20:57:52 +0100

    Bump TYPO3 requirement to 9.5.x in ext_emconf.php.

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.99.99',
+            'typo3' => '9.5.0-9.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
